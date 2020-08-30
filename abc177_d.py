1befd742b99b079bbfa2b937177642af7cce43df
burita083 <s.naka0523@gmail.com>
2020-08-30 23:55:02 +0900

    daily commit

--- abc177_d.py
+++ abc177_d.py
@@ -1,16 +1,40 @@
 N, M = map(int, input().split())
+AB = [tuple(map(int,input().split())) for i in range(M)]
 
-#AB = [tuple(map(int, input().split())) for i in range(M)]
-#丸暗記。隣接リストの初期化
-graph = [[i+1] for i in range(N)]
+class UnionFind:
+    def __init__(self,N):
+        self.parent = [i for i in range(N)]
+        self._size = [1] * N
+        self.count = 0
 
-for _ in range(M):
-  a, b = map(int, input().split())
-  graph[a-1].append(b)
-  graph[b-1].append(a)
+    def root(self,a):
+        if self.parent[a] == a:
+            return a
+        else:
+            self.parent[a] = self.root(self.parent[a])
+            return self.parent[a]
+    def is_same(self,a,b):
+        return self.root(a) == self.root(b)
 
-ans = -1
-for g in graph:
-  ans = max(ans, len(set(g)))
-print(graph)
+    def unite(self,a,b):
+        ra = self.root(a)
+        rb = self.root(b)
+        if ra == rb: return
+        if self._size[ra] < self._size[rb]: ra,rb = rb,ra
+        self._size[ra] += self._size[rb]
+        self.parent[rb] = ra
+        self.count += 1
+
+    def size(self,a):
+        return self._size[self.root(a)]
+
+uf = UnionFind(N)
+for a,b in AB:
+    a,b = a-1,b-1
+    if uf.is_same(a,b): continue
+    uf.unite(a,b)
+
+ans = 0
+for i in range(N):
+    ans = max(ans, uf.size(i))
 print(ans)
