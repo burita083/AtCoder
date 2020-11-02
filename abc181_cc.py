a4eff91a7a5c3f097a950f503b8c0589eab5c62c
burita083 <s.naka0523@gmail.com>
2020-11-02 23:55:02 +0900

    daily commit

--- abc181_cc.py
+++ abc181_cc.py
@@ -1,33 +1,18 @@
 N = int(input())
-import math
-A = []
-for _ in range(N):
-  x, y = map(int, input().split())
-  A.append((x, y))
+XY = [tuple(map(int,input().split())) for i in range(N)]
+
+for i in range(N-2):
+    for j in range(i+1,N-1):
+        for k in range(j+1,N):
+            x1,y1 = XY[i]
+            x2,y2 = XY[j]
+            x3,y3 = XY[k]
+            a = x2-x1
+            b = y2-y1
+            c = x3-x1
+            d = y3-y1
+            if a*d == b*c:
+                print('Yes')
+                exit()
+print('No')
  
-
-def get_distance(x1, y1, x2, y2):
-    d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-    return d
-
-for i in range(len(A)-2):
-  for j in range(i+1, len(A)-1):
-    for k in range(j+1, len(A)):
-      AB = get_distance(A[i][0], A[i][1], A[j][0], A[j][1])
-      AC = get_distance(A[i][0], A[i][1], A[k][0], A[k][1])
-      BC = get_distance(A[j][0], A[j][1], A[k][0], A[k][1])
-      
-      if (abs(AB) + abs(AC) == abs(BC)) or (abs(AB) + abs(BC) ==  abs(AC)) or (abs(AC) + abs(BC) == abs(AB)):
-        if AB**2 + AC**2 == BC**2:
-          continue
-
-        if AB**2 + BC**2 == AC**2:
-          continue
-
-        if AC**2 + BC**2 == AB**2:
-          continue
-
-        print("Yes")
-        exit()
-
-print("No")
