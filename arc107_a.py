97429f533e6f5dd05b50d0e591d69375179007ee
burita083 <s.naka0523@gmail.com>
2020-11-03 23:55:04 +0900

    daily commit

--- arc107_a.py
+++ arc107_a.py
@@ -1,15 +1,4 @@
 A, B, C = map(int, input().split())
-amari = 998244353
-a = A*(A+1)%amari
-b = B*(B+1)%amari
-c = C*(C+1)%amari
-a *= 0.5
-b *= 0.5
-c *= 0.5
-print(a)
-print(b)
-print(c)
-print(int(a*b*c)%amari)
-
+MOD = 998244353
 sum = A*(A+1)//2 * B*(B+1)//2 * C*(C+1)//2
-print(sum % 998244353)
+print(sum % MOD)
