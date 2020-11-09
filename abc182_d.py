5c27935619418bb8d9169f3c3eca5b8c5f503382
burita083 <s.naka0523@gmail.com>
2020-11-09 23:55:04 +0900

    daily commit

--- abc182_d.py
+++ abc182_d.py
@@ -3,27 +3,13 @@ N = int(input())
 A = list(map(int, input().split()))
 
 ans = 0
-from itertools import accumulate
-S = [0]+list(accumulate(A))
-m = max(S)
-index = S.index(max(S))
+p = 0
+m = 0
+d = 0
 
-for i in range(N-1):
-  ans += S[i+1]-S[0]
-
-for i in range(index):
-  ans += S[i]
-
-if ans >= 0:
-  print(ans)
-else:
-  print(0)
-#print(S)
-
-#for i in range(len(A)):
-  #for j in range(i+1):
-    #ans += A[j]
-    #mx = max(mx, ans)
-    #print(ans, i, "aaaa")
-
-#print(mx)
+for i in range(N):
+  d += A[i]
+  m = max(m, d)
+  ans = max(ans, p+m)
+  p += d
+print(ans)
