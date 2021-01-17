5847b6e7cb3586eee560dc97febc6598a9da20b2
burita083 <s.naka0523@gmail.com>
2021-01-17 23:55:05 +0900

    daily commit

--- abc138_d.py
+++ abc138_d.py
@@ -1,4 +1,6 @@
+
 from collections import deque
+
 N, Q = map(int, input().split())
 
 g = [[] for i in range(N)]
@@ -9,24 +11,18 @@ for _ in range(N-1):
   g[b-1].append(a-1)
 
 cum = [0]*N
-a = 0
 PQ = [tuple(map(int, input().split())) for i in range(Q)]
 for p, q in PQ:
-  a += q
   cum[p-1] += q
   
-queue = deque([0])
-ans = [0] * N
-ans[0] = cum[0]
-
 visited = [0] * N
 
-while queue:
-  v = queue.popleft()
-  visited[v] = 1
-  for e in g[v]:
-    if visited[e] == 1:
+def dfs(thisNode, prev=-1):
+  for nextNode in g[thisNode]:
+    if nextNode == prev:
       continue
-    cum[e] += cum[v]
-    queue.append(e)
+    cum[nextNode] += cum[thisNode]
+    dfs(nextNode, thisNode)
+
+dfs(0)
 print(*cum)
