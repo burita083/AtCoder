4dc7ac38b5b59201f6f2bc8ae7a0bb760577beb9
burita083 <s.naka0523@gmail.com>
2021-02-15 23:55:02 +0900

    daily commit

--- arc107_b.py
+++ arc107_b.py
@@ -1,7 +1,15 @@
 N, K = map(int, input().split())
 
+def count(n):
+    if n < 2 or n > 2*N: return 0
+    if n <= N+1:
+        return n-1
+    else:
+        return 2*N+1-n
 
-print(2525*2525)
-print(2100*2100)
-a = (5050-2)*(5050-427)
-print(a)
+ans = 0
+for x in range(2, 2*N+1):
+    Y = x-K
+    ans += count(x) * count(Y)
+print(ans)
+
