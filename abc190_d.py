51e17ec917cb532f99a6022309cc8da2c38c7456
burita083 <s.naka0523@gmail.com>
2021-02-20 23:55:00 +0900

    daily commit

--- abc190_d.py
+++ abc190_d.py
@@ -1,24 +1,17 @@
-N = int(input())
-
-
-N, K = map(int, input().split())
-
-A = []
-for i in range(20):
-  A.append(i)
-
-ans = 0
-r = 0
-s = 0
-l = []
-ans = []
-while (r < N)
-  s += A[r]
-  l.append(A[r])
-  r += 1
-  if s == N:
-    ans.append(l)
-    l = []
-    s -= A[l]
-
+N=int(input())
+def divisor(M):
+  res = set()
+  i=1
+  while(i*i<M):
+    if M%i==0:
+      res.add(i)
+      res.add(M//i)
+    i+=1
+  return res
+D=divisor(2*N)
+ans=0
+for n in D:
+  m = (2*N)//n
+  if (m-n)%2==1:
+    ans+=1
 print(ans)
